ea9dcc2f117539eaab019e2bfa83509c1fa3ff7a
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-09-12 13:38:47 -0400

    Organized files and added new vc

---
+++ CodePath-Flix/ViewControllers/MovieDetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MovieDetailViewController.swift
+//  CodePath-Flix
+//
+//  Created by Denielle Abaquita on 9/12/21.
+//
+
+import UIKit
+
+class MovieDetailViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
